a3a8fb2c5f4ec501b6a898528b817cde37a0ee36
AndreyI <andreyivanov86@gmail.com>
2020-04-07 11:07:25 -0400

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 class App {
   constructor() {
-    console.log('App works')
+    this.$form = document.querySelector('#form');
+    this.addEventListeners();
   }
+
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      handleFormClick(event);
+    })
+  }
+
+  handleFormClick(event) {
+
+  }
+
 }
 
 new App();
